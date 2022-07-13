4f8812f3211423b858e48e04c3a347666d1acde8
Zekeriya Şahin <barhar2906@gmail.com>
2022-07-13 17:54:07 +0300

    Click the button and change the picture
    Added codes to change pictures when clicking the button. Added ImageView for to see images.
    fixes: z-0001

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var MyImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        
+        if MyImageView.image == UIImage(named: "swift"){
+            
+            MyImageView.image = UIImage(named: "swiftCode")
+            
+        }else{
+            MyImageView.image = UIImage(named: "swift")
+        }
+    }
 }
 
